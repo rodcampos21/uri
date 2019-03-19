1a091d627ad6ceef3f0feb8d580e0d8de1549649
rodcampos21 <48729440+rodcampos21@users.noreply.github.com>
2019-03-19 14:15:10 -0300

    Add files via upload

---
+++ 1019.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int horas;
+        int minutos;
+        int segundos;
+        int tempo = 0;
+
+        string calculo;
+        calculo = Console.ReadLine();
+        tempo = int.Parse(calculo);
+
+        horas = tempo / 3600;
+        minutos = (tempo % 3600) / 60;
+        segundos = (tempo % 3600) % 60;
+
+        Console.WriteLine("" + horas + ":" + minutos + ":" + segundos);
+        Console.ReadLine();
+    }
+
+}
