fc46a15d8d3b758a90987f0ac0dc0b96602bf915
rodcampos21 <48729440+rodcampos21@users.noreply.github.com>
2019-03-19 14:13:47 -0300

    Add files via upload

---
+++ 1020.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int ano;
+        int mes;
+        int dia;
+        int idade = 0;
+
+        string calculo;
+        calculo = Console.ReadLine();
+        idade = int.Parse(calculo);
+
+        ano = idade / 365;
+        mes = (idade % 365) / 30;
+        dia = (idade % 365) % 30;
+
+        Console.WriteLine($"{ano} ano(s)\n{mes} mes(es)\n{dia} dia(s)");
+        Console.ReadLine();
+    }
+
+}
