7690bfa61dd216e85b81eff25a9517d64758ca9c
rodcampos21 <48729440+rodcampos21@users.noreply.github.com>
2019-03-20 10:02:09 -0300

    Add files via upload

---
+++ 1035.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A =0, B=0, C=0, D=0;
+
+
+            String[] vet = Console.ReadLine().Split(' ');
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+            C = int.Parse(vet[2]);
+            D = int.Parse(vet[3]);
+
+
+
+            if (B > C && D > A && C + D > A + B && A % 2 == 0)
+
+
+                  
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+
+                                
+                        
+            }
+
+            Console.ReadLine();
+
+        }
+        
+    }
+
+    
+}
